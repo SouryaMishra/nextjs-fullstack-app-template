aee5114406bf9059774f25be56e18a53e67e7ea4
Sourya Mishra <sourya.mishra@educationsoftwaresolutions.co.uk>
2022-10-06 23:37:03 +0530

    feat: add CatCard component

--- components/cards/cat/CatCard.mocks.ts
+++ components/cards/cat/CatCard.mocks.ts
@@ -1,7 +1,11 @@
 import { ICatCardProps } from "./CatCard";
 
 const base: ICatCardProps = {
-  text: "Hello World!",
+  tag: "Felines",
+  title: `What's new in Cats`,
+  body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!",
+  author: "SM",
+  time: "2h ago",
 };
 
 export const mockCatCardProps = {
